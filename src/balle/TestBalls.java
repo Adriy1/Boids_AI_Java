9688b0416f4f98cb3cfb0205a357ce1d9c12b874
Adrien <pietadrien@gmail.com"Lom13>
2017-10-28 19:26:02 +0200

    renaming

---
+++ src/balle/TestBalls.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+
+public class TestJava {
+
+
+  public static void main(String[] args) {
+    int dx=5,dy=5;
+    Point test = new Point(5,4);
+    Balls bb = new Balls(test,100);
+    int i;
+    for(i=0;i<100;i++) {
+      bb.translate(dx,dy);
+      System.out.println(bb.balle1.toString() + "DY: " + bb.DY + "et DX: " + bb.DX);
+      dx= bb.DX;
+      dy =bb.DY;
+    }
+
+
+
+
+  }
+}
